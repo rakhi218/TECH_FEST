968318ce9c827b195863ecce790cb56e3d18adf4
rakhi218 <rakeshvanga2180@gmail.com>
2023-02-15 22:51:00 +0530

    changed

--- app.js
+++ app.js
@@ -1,21 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var ejs = require('ejs');
-const sql = require('mssql');
-
-const config = {
-    user: 'azureUser', // better stored in an app setting such as process.env.DB_USER
-    password: 'Pass@123', // better stored in an app setting such as process.env.DB_PASSWORD
-    server: 'tech-fest-nittt.database.windows.net', // better stored in an app setting such as process.env.DB_SERVER
-    port: 1433, // optional, defaults to 1433, better stored in an app setting such as process.env.DB_PORT
-    database: 'TECH_FEST', // better stored in an app setting such as process.env.DB_NAME
-    authentication: {
-        type: 'default'
-    },
-    options: {
-        encrypt: true 
-    }
-}
 
 var app = express();
 var PORT = 3000;
@@ -28,9 +13,6 @@ app.get("/",(req,res) => {
     res.render("index");
 });
 
-app.get("/register",(req,res) => {
-    res.render("register.ejs");
-})
 
 app.listen(PORT,async() => {
     //var poolConnection = await sql.connect(config);
